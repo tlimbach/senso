7d98efa9d99fdd138e49d8194b86a5ad7c6b0481
Thorsten Limbach <tlimbach@miditec.local>
2019-04-10 08:25:11 +0200

    changed this behaviour

--- www/js/index.js
+++ www/js/index.js
@@ -3,44 +3,39 @@ var paper;
 
 function Light(name, x, y, colorOn, colorOff) {
     this.name = name;
-    log ("name -> " + name);
+    log("name -> " + name);
     this.circle = paper.circle(x, y, 100);
     this.circle.attr({
         fill: colorOn,
         cursor: 'pointer'
     });
 
-//   this.circle.click(msgBox(name +"  clicked"));  // geht
-
-   let that = this;
-   this.circle.click(function(){that.on(that)});  // Elegant is anders.... hm... ??!
-   
-//   this.circle.id = "cid";
-   
-//   $('#cid').on( 'click', this.on.bind(this) );
-
+    this.circle.click((function () {
+        this.on();
+    }).bind(this));
 }
 
-Light.prototype.on = function (light) {
-    log(light.name + " turned on");
+Light.prototype.on = function () {
+    log(this.name + " turned on");
 }
 
 Light.prototype.off = function () {
     log(this.name + " turned off");
 }
 
-function Device() {}
+function Device() {
+}
 
 Device.prototype.init = function () {
     this.circle = paper.circle(300, 300, 300);
     this.circle.attr({
         fill: 'gray'
     });
-    
-    this.greenlight = new Light("Greenlight", 200,200, 'green');
-    this.redlight = new Light("Redlight", 400,200, 'red');
-    this.yellowlight = new Light("Yellowlight", 200,400, 'yellow');
-    this.bluelight = new Light("Bluelight", 400,400, 'blue');
+
+    this.greenlight = new Light("Greenlight", 200, 200, 'green');
+    this.redlight = new Light("Redlight", 400, 200, 'red');
+    this.yellowlight = new Light("Yellowlight", 200, 400, 'yellow');
+    this.bluelight = new Light("Bluelight", 400, 400, 'blue');
 };
 
 $(document).ready(function () {
